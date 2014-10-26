baca9f67c5aeca04bb08f5112d33f8d4aa850fa5
haddock <bergljung@gmail.com>
2014-10-26 21:39:30 +0100

    Excercise 7 solved

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -9,22 +9,35 @@
     (reduce #(str %1 " " %2) a-seq)))
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (cond (empty? a-seq) '()
+        (empty? (next a-seq)) (seq a-seq)
+        :else (seq (reduce #(conj %1 x %2) (vector (first a-seq)) (rest a-seq)))))
 
 (defn my-count [a-seq]
-  :-)
+  (let [counter (fn [i elem]
+                  (inc i))]
+    (reduce counter 0 a-seq)))
 
 (defn my-reverse [a-seq]
-  [:-])
+  (reduce #(cons %2 %1) '() a-seq))
 
 (defn min-max-element [a-seq]
-  [:-])
+  (if (empty? a-seq)
+    a-seq
+    (let [min-max-reducer (fn [min-max elem]
+                            (vector
+                             (min (first min-max) elem)
+                             (max (last min-max) elem)))]
+      (reduce min-max-reducer [(first a-seq) (first a-seq)] (rest a-seq)))))
 
 (defn insert [sorted-seq n]
-  [:-])
+  (loop [result '() tmp-seq sorted-seq]
+    (if (or (empty? tmp-seq) (<= n (first tmp-seq)))
+      (concat result (cons n tmp-seq))
+      (recur (concat result (list (first tmp-seq))) (rest tmp-seq)))))
 
 (defn insertion-sort [a-seq]
-  [:-])
+  (reduce #(insert %1 %2) [] a-seq))
 
 (defn parity [a-seq]
   [:-])
